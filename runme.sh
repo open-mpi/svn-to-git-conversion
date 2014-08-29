ac6a720f43371d4904b3fcca06c90d288488ef6f
Jeff Squyres <jsquyres@cisco.com>
2014-08-29 12:19:40 -0700

    Re-org; use git fetch in a loop, even if we un-tarred

--- runme.sh
+++ runme.sh
@@ -9,17 +9,27 @@ rname=ompi-svn-to-git
 rdir=$base/$rname
 svn_url=https://svn.open-mpi.org/svn/ompi
 
+do_svn_init=1
+
+cd $base
+
+# Sanity check
+if test $do_svn_init -eq 0 && \
+    test ! -r ompi-svn-to-git-1-after-clone.tar.bz2; then
+    echo "***  WARNING: Selected NOT to re-init SVN, but there's no tarball!"
+    echo "*** Cannot continue"
+    exit 1
+fi
+
+##########################################################################
+
 # Make sure we have the latest / greatest
 module unload cisco/git
 module load cisco/git/2.1.0
 
-cd $base
 rm -rf $rname
 
-do_svn_clone=1
-num_fails=0
-
-if test "$do_svn_clone" -eq 1; then
+if test "$do_svn_init" -eq 1; then
     # Do the git svn clone
     start=`date`
     echo "===== STARTING: $start"
@@ -29,6 +39,7 @@ if test "$do_svn_clone" -eq 1; then
 
     git config svn.authorsfile $scripts_dir/authors.txt
 
+    # Get the right refs for OMPI's wonky SVN tag layout
     git config --replace-all svn-remote.svn.tags \
         'tags/v1.0-series/*:refs/remotes/tags/*'
     git config --add svn-remote.svn.tags \
@@ -47,66 +58,58 @@ if test "$do_svn_clone" -eq 1; then
         'tags/v1.7-series/*:refs/remotes/tags/*'
     git config --add svn-remote.svn.tags \
         'tags/v1.8-series/*:refs/remotes/tags/*'
-
-    done=0
-    while test $done -eq 0; do
-        git svn fetch --all
-        st=$?
-        if test $st -eq 0; then
-            echo '======== git svn completed successfully!!'
-            done=1
-        else
-            echo '======== git svn failed.  looping...'
-            num_fails=`expr $num_fails + 1`
-            if test $num_fails -gt 20; then
-                stop=`date`
-                echo "====== hmmm... lotsa failures... exiting..."
-                echo "===== GIT SVN CONVERT FAILED"
-                echo "===== START: $start"
-                echo "===== STOP:  $stop"
-                pushover SVN git clone failed
-                exit 1
-            fi
-            sleep 2
-        fi
-    done
-    stop=`date`
-    echo "===== GIT SVN CONVERT DONE"
-    echo "===== START: $start"
-    echo "===== STOP:  $stop"
-
-    # Save step 1
-    echo '=========== SAVING GIT SVN CLONE'
-    tar jcf $rname-1-after-clone.tar.bz2 $rname
 else
     # Extract the git svn clone
     tar xf $rname-1-after-clone.tar.bz2
+    cd $rname
+    git checkout master
 fi
-cd $rname
 
-# Get the latest.
-# Just to master and v1.8 branch; as of this writing (Aug 2014), those
-# are the only 2 SVN branches will be be changing.
-echo ----------- Running git svn rebase
-git checkout master
-git svn rebase
-git checkout -b convert-v1.8 remotes/v1.8
-git svn rebase
-git checkout master
+# Loop over git svn fetch (because sometimes the network between here
+# and upstream goes out, falsely causing "git svn fetch" to fail).
+num_fails=0
+done=0
+while test $done -eq 0; do
+    git svn fetch --all
+    st=$?
+    if test $st -eq 0; then
+        echo '======== git svn completed successfully!!'
+        done=1
+    else
+        echo '======== git svn failed.  looping...'
+        num_fails=`expr $num_fails + 1`
+        if test $num_fails -gt 20; then
+            stop=`date`
+            echo "====== hmmm... lotsa failures... exiting..."
+            echo "===== GIT SVN CONVERT FAILED"
+            echo "===== START: $start"
+            echo "===== STOP:  $stop"
+            pushover SVN git clone failed
+            exit 1
+        fi
+        sleep 2
+    fi
+done
+stop=`date`
+echo "===== GIT SVN CONVERT DONE"
+echo "===== START: $start"
+echo "===== STOP:  $stop"
 
+# Save step 1
 cd ..
-echo '============ SAVING AFTER GIT SVN REBASE'
-tar jcf $rname-1.5-after-svn-rebase.tar.bz2 $rname
+echo '=========== SAVING GIT SVN CLONE'
+tar jcf $rname-1-after-clone.tar.bz2 $rname
 cd $rname
 
-# Delete the "tim" and "orte" branches; we don't need those in
+# Delete the "tim" and "orte" branches; we don't need those in the
+# final OMPI git repos.
+echo ----------- Deleting stale branches
+git branch -D tim
+git branch -D orte
 
 # Run the preprocess step
 echo ----------- Running preprocess
 $scripts_dir/preprocess-repo.sh
-
-# Kill the branch we made above to rebase
-git branch -D convert-v1.8
 
 # Save step 2
 cd ..
@@ -116,7 +119,11 @@ tar jcf $rname-2-after-preprocess.tar.bz2 $rname
 echo ----------- Running git filter-branch
 cd $rname
 rm -f /tmp/gfb-$USER.log
-# JMS Dave would like to see the error message from git 1.8.5.x here
+
+# This version of git works with our git filter-branch command
+module unload cisco/git
+module load cisco/git/1.8.2.1
+
 git filter-branch --commit-filter "$scripts_dir/convert-ompi.pl"' "$@"' --tag-name-filter cat -- --all --date-order
 git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d
 
